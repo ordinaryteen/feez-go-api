6224b7b75cb3de68a55064629f17821860cb7be6
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-17 22:05:32 +0700

    integrate middleware and cart into main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,7 +9,9 @@ import (
 
 	// import the  database
 	"github.com/ordinaryteen/feez-go-api/internal/auth"
+	"github.com/ordinaryteen/feez-go-api/internal/cart"
 	_ "github.com/ordinaryteen/feez-go-api/internal/database"
+	"github.com/ordinaryteen/feez-go-api/internal/middleware"
 	"github.com/ordinaryteen/feez-go-api/internal/product"
 )
 
@@ -20,9 +22,16 @@ func main() {
 		w.Write([]byte("Hello, feez API! Server is running!"))
 	})
 
+	// --- API v1 (Public Routes) ---
 	r.Post("/api/v1/signup", auth.HandleSignup)
 	r.Post("/api/v1/login", auth.HandleLogin)
 	r.Get("/api/v1/products", product.HandleGetProducts)
+
+	// --- API v1 (Private Routes) ---
+	r.Group(func(r chi.Router) {
+		r.Use(middleware.AuthMiddleware)
+		r.Post("/api/v1/cart", cart.HandleAddToCart)
+	})
 
 	port := ":8080"
 	fmt.Println("Server listening on port", port)
