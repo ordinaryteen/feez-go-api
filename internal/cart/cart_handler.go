1dcd7c6cf9022c546522717e31a2d02dabea4ded
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-17 22:11:39 +0700

    adds cart-checking function in cart_handler.go

--- internal/cart/cart_handler.go
+++ internal/cart/cart_handler.go
@@ -14,6 +14,68 @@ type AddToCartRequest struct {
 	Quantity  int   `json:"quantity"`
 }
 
+type CartItemResponse struct {
+	ProductID    int64  `json:"product_id"`
+	ProductName  string `json:"product_name"`
+	Quantity     int    `json:"quantity"`
+	PricePerItem int    `json:"price_per_item"`
+}
+
+func HandleGetCart(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	userID, ok := middleware.GetUserIDFromContext(ctx)
+	if !ok {
+		http.Error(w, "User ID not found in context", http.StatusInternalServerError)
+		return
+	}
+
+	sqlQuery := `
+		SELECT 
+			c.product_id, 
+			p.name, 
+			c.quantity, 
+			p.price
+		FROM 
+			public.cart_items c
+		JOIN 
+			public.products p ON c.product_id = p.id
+		WHERE 
+			c.user_id = $1
+	`
+
+	rows, err := database.DB.Query(context.Background(), sqlQuery, userID)
+	if err != nil {
+		http.Error(w, "Failed to fetch cart items", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	cartItems := []CartItemResponse{}
+
+	for rows.Next() {
+		var item CartItemResponse
+		if err := rows.Scan(
+			&item.ProductID,
+			&item.ProductName,
+			&item.Quantity,
+			&item.PricePerItem,
+		); err != nil {
+			http.Error(w, "Failed to process cart data", http.StatusInternalServerError)
+			return
+		}
+		cartItems = append(cartItems, item)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, "Error reading cart data", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(cartItems)
+}
+
 func HandleAddToCart(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
@@ -33,7 +95,6 @@ func HandleAddToCart(w http.ResponseWriter, r *http.Request) {
 		req.Quantity = 1
 	}
 
-	// Integrasi ke DB
 	sqlQuery := `
 		INSERT INTO public.cart_items (user_id, product_id, quantity)
 		VALUES ($1, $2, $3)
