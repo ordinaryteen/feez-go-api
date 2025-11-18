07e778cce57800ac5d6a12bd5e43630c9a0a9dfb
nafiMulyo <muhamadnafimulyo@gmail.com>
2025-11-18 19:09:07 +0700

    Feat: Add React client structure (Vite + Chakra UI)

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// 1. Import komponen 'Heading'
+import { Heading } from '@chakra-ui/react';
+
+function App() {
+  // 2. TAMPILIN 'Heading' DOANG. Gak pake 'Box', gak pake 'bg="dark"'.
+  return (
+    <Heading>APAKAH INI MUNCUL?</Heading>
+  );
+}
+
+export default App;
